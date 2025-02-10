f3f5bfd8bc2aeea982fdc12ea362a280f1ccda53
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-02-10 07:46:23 -0600

    Finish routing part of news application

--- app/components/news-list.js
+++ app/components/news-list.js
@@ -1,19 +1,15 @@
 import Link from 'next/link';
 
 export default function NewsList({ news }) {
-  return (
-    <ul className="news-list">
-      {news.map(newsItem => (
-        <li key={newsItem.id}>
-          <Link href={`/news/${newsItem.slug}`}>
-            <img
-              alt={newsItem.title}
-              src={`/images/news/${newsItem.image}`}
-            />
-            {newsItem.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+  return news.map(newsItem => (
+    <li key={newsItem.id}>
+      <Link href={`/news/${newsItem.slug}`}>
+        <img
+          alt={newsItem.title}
+          src={`/images/news/${newsItem.image}`}
+        />
+        {newsItem.title}
+      </Link>
+    </li>
+  ))
 }
