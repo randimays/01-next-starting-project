9b7836dcd63e58a9fe77904d0317c95467df02cd
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-02-10 08:00:42 -0600

    Getting started with data fetching in the News application

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,50 @@
-import { DUMMY_NEWS } from '@/dummy-news';
+'use client';
+
 import NewsList from '@/app/components/news-list';
+import { useEffect, useState } from 'react';
 
 const News = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      setIsLoading(true);
+
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setError('Failed to fetch news.');
+      }
+
+      setIsLoading(false);
+
+      const news = await response.json();
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News</h1>
-      <ul className="news-list">
-        <NewsList news={DUMMY_NEWS} />
-      </ul>
+      {newsContent}
     </>
   );
 };
--- app/components/news-list.js
+++ app/components/news-list.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 
 export default function NewsList({ news }) {
-  return news.map(newsItem => (
-    <li key={newsItem.id}>
-      <Link href={`/news/${newsItem.slug}`}>
-        <img
-          alt={newsItem.title}
-          src={`/images/news/${newsItem.image}`}
-        />
-        {newsItem.title}
-      </Link>
-    </li>
-  ))
+  return (
+    <ul className="news-list">
+      {news.map(newsItem => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <img
+              alt={newsItem.title}
+              src={`/images/news/${newsItem.image}`}
+            />
+            {newsItem.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
