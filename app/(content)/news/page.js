67b83e8aff399e9026b279393d4a5770eba11516
Randi Mays <19175324+randimays@users.noreply.github.com>
2025-02-12 14:28:34 -0600

    Refactor to do server-side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,50 +1,13 @@
-'use client';
-
 import NewsList from '@/app/components/news-list';
-import { useEffect, useState } from 'react';
+import { getAllNews } from '@/app/lib/news';
 
-const News = () => {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState(null);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      setIsLoading(true);
-
-      const response = await fetch('http://localhost:8080/news');
-
-      if (!response.ok) {
-        setError('Failed to fetch news.');
-      }
-
-      setIsLoading(false);
-
-      const news = await response.json();
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+const News = async () => {
+  const news = getAllNews();
 
   return (
     <>
       <h1>News</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 };
